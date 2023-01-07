7ded3bdd98fa342256efbd6fac6fff811bd64f2b
adhewale66@gmail.com <adhewale66@gmail.com>
2023-01-07 14:09:32 +0530

    vbn

---
+++ src/main/java/Banking/sbi/App.java
@@ -0,0 +1,15 @@
+package Banking.sbi;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class App 
+{
+	static Logger logger = LogManager.getLogger(App.class);
+    public static void main( String[] args )
+    {
+        System.out.println("hello");
+        logger.info("iformation");
+        logger.error("error");
+    }
+}
